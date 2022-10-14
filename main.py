92c4d82207f3520e81f94e0926a32f45968fca30
Ahmad Muhajir <jeremyjeferson71@gmail.com>
2022-10-14 19:19:16 +0700

    add comment and change some detail

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import json, os
 
+# save.json location
 svdfile = './saved/save.json'
 
 def newFile():
     os.system('cls')
     players = input('Players (separate with comma (,)):\n').split(',')
 
+    # Insert players into playersdict with each key with a value of 0
     playersdict = dict()
     for player in players:
         playersdict[player] = 0
+
+    # Declare load and saveslot as global variable
     global load, saveslot
     saveslot = -1
     load = playersdict
     playFile()
 
 def saveFile(x):
+    # When you make a new file, you will go to this if
     if x == -1:
+        # Read save.json and append the data
         with open(svdfile, 'r') as data:
             info = json.load(data)
             info.append(load)
@@ -23,14 +29,19 @@ def saveFile(x):
             global saveslot
             saveslot = len(info)-1
 
+        # Write changes into save.json
         with open(svdfile, 'w') as data:
             data.write(json_object)
+
+    # When you load a file, you will go to this else
     else:
+        # Read save.json and changes the data from index
         with open(svdfile, 'r') as data:
             info = json.load(data)
             info[saveslot] = load
             json_object = json.dumps(info, indent=4)
 
+        # Write changes into save.json
         with open(svdfile, 'w') as data:
             data.write(json_object)
 
@@ -39,14 +50,17 @@ def loadFile():
     with open(svdfile, 'r') as data:
         info = json.load(data)
         print('-------Saved Files-------')
+        # If there is no data in save.json
         if len(info) == 0:
             print('No files found')
             os.system('pause')
             return
+        # If there is data in save.json
         else:
             for i in range(1, len(info)+1):
                 print(f'[{i}] Save{i}')
 
+            # Declare load and saveslot as global variable
             global load, saveslot
             saveslot = int(input('Load> '))-1
             load = info[saveslot]
@@ -58,10 +72,12 @@ def deleteFile():
         with open(svdfile, 'r') as data:
             info = json.load(data)
             print('-------Delete File-------')
+            # If there is no data in save.json
             if len(info) == 0:
                 print('No files found')
                 os.system('pause')
                 return
+            # If there is data in save.json
             else:
                 for i in range(1, len(info)+1):
                     print(f'[{i}] Save{i}')
@@ -70,19 +86,20 @@ def deleteFile():
                     answer = int(input('Delete> '))-1
                 except:
                     continue
-
+                # Back
                 if answer == -1:
                     return
-                elif answer == range(len(info)):
+                # Delete file
+                elif answer in range(len(info)):
                     while True:
-                        surely = input('Are you sure?[y/n]: ')
-                        if surely == 'y':
+                        confirmation = input('Are you sure?[y/n]: ')
+                        if confirmation == 'y':
                             info.pop(answer)
                             json_object = json.dumps(info, indent=4)
                             with open(svdfile, 'w') as data:
                                 data.write(json_object)
                             break
-                        elif surely == 'n':
+                        elif confirmation == 'n':
                             return
                         else:
                             continue
